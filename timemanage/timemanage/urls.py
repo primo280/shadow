859f03a26995d21f325c95b6bc4df9b1c674eb47
primo280 <dohaprimael06@gmail.com>
2025-01-31 16:55:33 -0500

    tow base.html'

--- timemanage/timemanage/urls.py
+++ timemanage/timemanage/urls.py
@@ -6,4 +6,5 @@ from time_table_manage import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home , name="home"),
+    path('/register', views.register , name="register"),
 ]
