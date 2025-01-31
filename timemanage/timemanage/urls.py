39502692ed11ea248585be6ad26b46b698aa9498
primo280 <dohaprimael06@gmail.com>
2025-01-31 16:47:36 -0500

    initial

---
+++ timemanage/timemanage/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from time_table_manage import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home , name="home"),
+]
